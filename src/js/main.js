7e771e4014e7aa420339368876ccbe588b6b8d18
Brian Harvey <brian.harv3y@gmail.com>
2020-09-12 15:55:24 -0700

    continue troubleshotting jquery

--- src/js/main.js
+++ src/js/main.js
@@ -6,27 +6,29 @@ import ExchangeService from './exchange.js';
 
 function clearFields() {
   $("#userAmount").val(" ");
-  $("#currency-select").val("sel");
+  $("#currency-select").val("0");
 }
 
 function getElements(response) {
   let amount = $("#userAmount").val();
-  if ($("#currency-select option:selected").text() === "1") {
-    $("#currency-output").text(`The value of ${amount} in Australian Dollars is AUD ${response.conversion_rates.AUD}`);
-    console.log(response.conversion_rates);
-  } else {
-    console.log(response.conversion_rates.AUD);
-  }
+  //if ($("#currency-select option:selected").val() === 1) {
+    $("#currency-output").text(`The value of ${amount} in Australian Dollars is AUD ${amount * response.conversion_rates.AUD}`);
+    
+  //} else {
+    console.log(`${response.conversion_rates.AUD}`);
+    console.log(`${amount}`);
+ // }
 }
 
 
 $(document).ready(function() {
-  $("#confirm").click(function() {
+  $("#input-amount").submit(function(event) {
+    event.preventDefault();
     let amount = $("#userAmount").val();
-    clearFields();
-    ExchangeService.getCurrency()
+    ExchangeService.getCurrency(amount)
       .then(function(response) {
-        getElements(response);
+        getElements(response);   
       });
+    clearFields();  
   });
 });
