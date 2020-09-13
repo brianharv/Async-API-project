6abf4c3256a6b3c98386eb397bddfa461aac5958
Brian Harvey <brian.harv3y@gmail.com>
2020-09-12 18:13:51 -0700

    meet functionality objectives from project requirements

--- src/js/main.js
+++ src/js/main.js
@@ -5,28 +5,38 @@ import '../css/styles.css';
 import ExchangeService from './exchange.js';
 
 function clearFields() {
-  $("#userAmount").val(" ");
-  $("#currency-select").val("sel");
+  $("#reset").click(function() { 
+    $("#userAmount").val(" ");
+    $("#currency-select").val("0");
+  })
 }
 
 function getElements(response) {
   let amount = $("#userAmount").val();
-  if ($("#currency-select option:selected").text() === "1") {
-    $("#currency-output").text(`The value of ${amount} in Australian Dollars is AUD ${response.conversion_rates.AUD}`);
-    console.log(response.conversion_rates);
+  if ($("#currency-select option:selected").val() == "1") {
+    $("#currency-output").text(`The value of ${amount} USD in Australian Dollars is AUD ${amount * response.conversion_rates.AUD}`);
+  } else if ($("#currency-select option:selected").val() == "2") {
+    $("#currency-output").text(`The value of ${amount} USD in Bulgarian Lev is BGN ${amount * response.conversion_rates.BGN}`);
+  } else if ($("#currency-select option:selected").val() == "3") {
+    $("#currency-output").text(`The value of ${amount} USD in Canadian Dollars is CAD ${amount * response.conversion_rates.CAD}`);
+  } else if ($("#currency-select option:selected").val() == "4") {
+    $("#currency-output").text(`The value of ${amount} USD in Euros is EUR ${amount * response.conversion_rates.EUR}`);
+  } else if ($("#currency-select option:selected").val() == "5") {
+    $("#currency-output").text(`The value of ${amount} USD in Pounds Sterling is GBP ${amount * response.conversion_rates.GBP}`);
+  } else if (amount == "0" || $("#currency-select option:selected").val() == "0") {
+    $("#showError").text("Please enter a value above 0 and select a currency from the dropdown box");
   } else {
-    console.log(response.conversion_rates.AUD);
-  }
+    $("#showError").text(`There was an error: ${response.message}`);
+  }  
 }
 
 
 $(document).ready(function() {
   $("#confirm").click(function() {
-    let amount = $("#userAmount").val();
-    clearFields();
     ExchangeService.getCurrency()
       .then(function(response) {
         getElements(response);
-      });
+      }); 
+    clearFields();  
   });
 });
